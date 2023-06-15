8fcfb27afe963bcf8af503bd13fd5cc05243c990
H4ckermed <medcraft61@gmail.com>
2023-06-15 10:37:03 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-  *main - Entry Point
-  *return: always 0 (success)
-  */
-int main(vaoid)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
+
